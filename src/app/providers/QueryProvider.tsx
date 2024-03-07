1f1eeff6de4322b0c6be639591eda61a1fc2598c
kokoro-hart <tobita@wd-flat.com>
2024-03-07 23:30:38 +0900

    refactor

--- src/app/providers/QueryProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { PropsWithChildren } from "react";
-
-import { queryClient } from "@/app/libs/query";
-
-export const QueryProvider = ({ children }: PropsWithChildren) => {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
-};
